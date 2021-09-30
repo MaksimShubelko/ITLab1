7e24e1b52641e5db352873086aade0ea5a550967
MaksimShubelko <86020437+MaksimShubelko@users.noreply.github.com>
2021-09-30 21:41:57 +0300

    Delete Lesson15 directory

--- Lesson15/src/main/java/com/home/utils/Printer.java
+++
@@ -1,8 +0,0 @@
-package com.home.utils;
-
-public abstract class Printer {
-
-    public static void print(String string) {
-        System.out.println(string);
-    }
-}
--- Lesson15/src/main/java/com/home/utils/TypesSorts.java
+++
@@ -1,7 +0,0 @@
-package com.home.utils;
-
-public enum TypesSorts {
-    ComparePriceMinMax,
-    ComparePriceMaxMin,
-    CompareAddingNewOld;
-}
